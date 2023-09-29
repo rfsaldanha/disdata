aaf6c89b17e102d395252d0626507850732af9ea
Raphael Saldanha <rfsaldanha@gmail.com>
2023-09-29 16:40:54 +0200

    Checkmate assertions

--- R/agg_data.R
+++ R/agg_data.R
@@ -5,7 +5,7 @@
 #' @param x a `tibble`.
 #' @param g_var Grouping variable name.
 #' @param d_var Date variable. Must be of class `Date`.
-#' @param a_unit Aggregate unit. `day`, `week` or `year`.
+#' @param a_unit Aggregate unit. `day`, `week`, or `year`.
 #' @param ... arguments passed to `timetk::summarise_by_time` function, including `.week_start`.
 #'
 #' @return a `tibble`.
@@ -17,6 +17,11 @@
 #' agg_data(x = dengue_rj, g_var = "ID_MN_RESI", d_var = "DT_SIN_PRI", a_unit = "week")
 #'
 agg_data <- function(x, g_var, d_var, a_unit, ...){
+  checkmate::assert_choice(g_var, choices = names(x))
+  checkmate::assert_choice(d_var, choices = names(x))
+  checkmate::assert_class(get({d_var}, x), classes = "Date")
+  checkmate::assert_choice(a_unit, choices = c("day", "week", "month", "year"))
+
   x %>%
     dplyr::mutate(original_row = TRUE) %>%
     dplyr::group_by(!!dplyr::sym(g_var)) %>%
--- tests/testthat/test-agg_data.R
+++ tests/testthat/test-agg_data.R
@@ -4,3 +4,10 @@ test_that("agg_data works", {
 
   expect_equal(nrow(res), 5459)
 })
+
+test_that("agg_data not works", {
+
+  expect_error(agg_data(x = dengue_rj, g_var = "aaa", d_var = "DT_SIN_PRI", a_unit = "week"))
+  expect_error(agg_data(x = dengue_rj, g_var = "ID_MN_RESI", d_var = "aaa", a_unit = "week"))
+  expect_error(agg_data(x = dengue_rj, g_var = "ID_MN_RESI", d_var = "aaa", a_unit = "quarter"))
+})
