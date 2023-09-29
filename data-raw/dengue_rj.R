a89f70fa12ccab0aa8484441df0127677ca43379
Raphael Saldanha <rfsaldanha@gmail.com>
2023-09-29 18:05:13 +0200

    Less variables

--- data-raw/dengue_rj.R
+++ data-raw/dengue_rj.R
@@ -2,9 +2,18 @@
 
 # dengue_rj <- microdatasus::fetch_datasus(year_start = 2010, year_end = 2010, uf = "RJ", information_system = "SINAN-DENGUE")
 
+important_vars <- c("ID_AGRAVO", "DT_NOTIFIC", "ID_UNIDADE",
+                    "DT_SIN_PRI", "CS_SEXO", "CS_GESTANT",
+                    "CS_RACA", "CS_ESCOL_N", "ID_MN_RESI",
+                    "COUFINF", "COMUNINF", "ID_OCUPA_N",
+                    "DT_SORO", "RESUL_SORO", "SOROTIPO",
+                    "CLASSI_FIN", "CRITERIO", "EVOLUCAO",
+                    "DT_OBITO", "HOSPITALIZ", "DT_INTERNA")
+
 dengue_rj <- arrow::read_parquet(file = "../dengue/dengue_data_files/dengue_data/parquets/dengue_2020.parquet") %>%
-  dplyr::filter(substr(ID_MUNICIP, 0, 2) == "33") %>%
+  dplyr::select(dplyr::all_of(important_vars)) %>%
+  dplyr::filter(substr(ID_MN_RESI, 0, 2) == "33") %>%
   dplyr::mutate(DT_SIN_PRI = lubridate::as_date(DT_SIN_PRI)) %>%
-  dplyr::filter(DT_SIN_PRI >= as.Date("2020-01-01") & DT_SIN_PRI <= as.Date("2020-12-31"))
+  dplyr::filter(DT_SIN_PRI >= as.Date("2019-01-01") & DT_SIN_PRI <= as.Date("2021-12-31"))
 
 usethis::use_data(dengue_rj, overwrite = TRUE)
