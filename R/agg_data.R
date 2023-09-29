ed9a274e6bc55c4e46c55b408cba1f777092854e
Raphael Saldanha <rfsaldanha@gmail.com>
2023-09-29 16:44:45 +0200

    Function comments

--- R/agg_data.R
+++ R/agg_data.R
@@ -17,25 +17,32 @@
 #' agg_data(x = dengue_rj, g_var = "ID_MN_RESI", d_var = "DT_SIN_PRI", a_unit = "week")
 #'
 agg_data <- function(x, g_var, d_var, a_unit, ...){
+  # Check assertions
   checkmate::assert_choice(g_var, choices = names(x))
   checkmate::assert_choice(d_var, choices = names(x))
   checkmate::assert_class(get({d_var}, x), classes = "Date")
   checkmate::assert_choice(a_unit, choices = c("day", "week", "month", "year"))
 
   x %>%
+    # Create variable to separate original from padded rows
     dplyr::mutate(original_row = TRUE) %>%
+    # Group by grouping variable
     dplyr::group_by(!!dplyr::sym(g_var)) %>%
+    # Pad rows by day, considering minimum and maximum date values
     timetk::pad_by_time(
       .date_var = !!dplyr::sym(d_var),
       .by = "day",
       .start_date = min(get({d_var}, x)),
       .end_date = max(get({d_var}, x))
     ) %>%
+    # Count original rows that represent cases (not padded rows)
+    # by a unit of time
     timetk::summarise_by_time(
       .date_var = !!dplyr::sym(d_var),
       .by = a_unit,
       freq = sum(.data$original_row, na.rm = TRUE),
       ...
     ) %>%
+    # Ungroup
     dplyr::ungroup()
 }
