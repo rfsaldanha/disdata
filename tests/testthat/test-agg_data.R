946927f52e722801bff5d272da6c1ac3746c363d
Raphael Saldanha <rfsaldanha@gmail.com>
2023-10-02 13:52:21 +0200

    Test correction

--- tests/testthat/test-agg_data.R
+++ tests/testthat/test-agg_data.R
@@ -2,7 +2,7 @@ test_that("agg_data works", {
 
   res <- agg_data(x = dengue_rj, g_var = "ID_MN_RESI", d_var = "DT_SIN_PRI", a_unit = "week")
 
-  expect_equal(nrow(res), 5459)
+  expect_equal(nrow(res), 8820)
 })
 
 test_that("agg_data not works", {
